7bcdc3e16096c5e443ac936ec365edd884908279
Brinda <154052901+Brinda-07@users.noreply.github.com>
2025-10-05 08:01:58 +0530

    Create ContainsDuplicate.cpp

---
+++ ContainsDuplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(auto it:nums){
+            if(mp[it]>=1){
+                return true;
+                break;
+            }
+            mp[it]++;
+        }
+        return false;
+        
+    }
+};
