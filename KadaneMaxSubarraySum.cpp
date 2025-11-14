09d3ae88e295f9ba3b42f34e55d21b0fde7c6e19
Brinda <154052901+Brinda-07@users.noreply.github.com>
2025-11-14 18:10:46 +0530

    Create KadaneMaxSubarraySum.cpp

---
+++ KadaneMaxSubarraySum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int maxSubarraySum(vector<int> &arr) {
+        // Code here
+        int ans=INT_MIN;
+        int sum=0;
+        for(int i=0;i<arr.size();i++)
+        {
+            sum=sum+arr[i];
+            ans=max(ans,sum);
+            if(sum<0)
+            {
+                sum=0;
+                
+            }
+            
+        }
+        return ans;
+    }
+};
