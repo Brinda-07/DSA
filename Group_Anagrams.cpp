2eafd823c9578219fe26e57ad7c5842721f38fe8
Brinda <154052901+Brinda-07@users.noreply.github.com>
2026-01-31 09:25:08 +0530

    Create Group_Anagrams.cpp

---
+++ Group_Anagrams.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>>ans;
+        unordered_map<string,vector<string>>mp;
+        for(auto it: strs)
+        {
+            string word=it;
+            sort(word.begin(),word.end());
+            mp[word].push_back(it);
+        }
+        for(auto it:mp)
+        {
+            ans.push_back(it.second);
+        }  
+        return ans;
+        
+    }
+};
