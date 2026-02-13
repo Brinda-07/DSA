50ff4abc45d04f2513279a6b875d2f31563f9fb1
Brinda <154052901+Brinda-07@users.noreply.github.com>
2026-02-13 11:08:05 +0530

    Create  Merge Adjacent Equal Elements.cpp

---
+++ Merge Adjacent Equal Elements.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<long long> mergeAdjacent(vector<int>& nums) {
+        stack<long long>st;
+
+        for(int i=0;i<nums.size();i++)
+        {
+           
+            long long x=nums[i];
+            while(!st.empty() && st.top()==x)
+            {
+                x=x*2;
+                st.pop();
+
+            }
+            st.push(x);
+        }
+        vector<long long>ans;
+        while(!st.empty())
+        {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+        
+    }
+};
