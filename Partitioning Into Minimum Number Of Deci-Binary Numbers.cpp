402354b8df2c83bf3ea6392439e748775c529c03
Brinda <154052901+Brinda-07@users.noreply.github.com>
2026-02-16 14:18:01 +0530

    Create Partitioning Into Minimum Number Of Deci-Binary Numbers.cpp

---
+++ Partitioning Into Minimum Number Of Deci-Binary Numbers.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minPartitions(string n) {
+        
+        int maxi=INT_MIN;
+        for(auto it :n)
+        {
+            maxi=max(maxi,it-'0');
+        }
+        return maxi;
+    }
+};
