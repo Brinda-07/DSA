8e501b2505fd363d381237987b3b98589235be7d
Brinda <154052901+Brinda-07@users.noreply.github.com>
2026-07-21 08:33:37 +0530

    Create Letter Case Permutation.cpp

---
+++ Letter Case Permutation.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+void solve(int ind,string &s,vector<string>&ans)
+{
+    if(ind==s.size())
+    {
+        ans.push_back(s);
+        return;
+    }
+    if(isalpha(s[ind]))
+    {
+        s[ind]=toupper(s[ind]);
+        solve(ind+1,s,ans);
+        s[ind]=tolower(s[ind]);
+        solve(ind+1,s,ans);
+    }
+    else
+    solve(ind+1,s,ans);
+
+
+}
+    vector<string> letterCasePermutation(string s) {
+        
+        vector<string>ans;
+        solve(0,s,ans);
+        return ans;
+        
+    }
+};
